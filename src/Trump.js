f67650f8a83bae81a948889b05150bdf9cb85955
Jonny van Essen <jonathan.vanessen@gmail.com>
2021-04-27 11:26:56 -0400

    got quotes working

--- src/Trump.js
+++ src/Trump.js
@@ -4,64 +4,36 @@ import axios from 'axios'
 
 
 function Trump(props) {
-  const [speaker, setSpeaker] = useState([])
-  // const [speech, setSpeech] = useState('')
+  const [quote, setQuote] = useState('')
   // const [loading, setLoading] = useState(false)
-  let msg;
-  let voices;
 
-  async function getSpeakers() {
-    msg = new SpeechSynthesisUtterance();
-    voices = await window.speechSynthesis.getVoices();
+  async function getTrumpQuotes() {
+    const response = await axios.get('http://tronalddump.io/random/quote')
+    setQuote(response.data.value)
+    console.log(response)
   }
 
+
+  // async function getSpeakers() {
+  //   msg = new SpeechSynthesisUtterance();
+  //   voices = await window.speechSynthesis.getVoices();
+  // }
+
   useEffect(() => {
-    getSpeakers()
+    getTrumpQuotes()
+    // getSpeakers()
   }, [])
 
-  function speak() {
-    msg.voice = voices[Math.floor(Math.random() * voices.length)];
-    msg.text = "I am speaking";
+  async function speak() {
+    // msg.voice = voices[Math.floor(Math.random() * voices.length)];
+    let msg = new SpeechSynthesisUtterance();
+    let voices = await window.speechSynthesis.getVoices();
+    msg.voice = voices[0];
+    msg.text = quote;
     window.speechSynthesis.speak(msg);
   }
 
 
-
-  // let voices = window.speechSynthesis.getVoices();
-  // window.speechSynthesis.onvoiceschanged = function () {
-
-  // }
-  // setSpeaker(msg.voices)
-
-  // setTimeout(() => {
-  //   console.log(msg);
-  //   console.log(voices);
-  // }, 500)
-
-
-
-
-
-
-
-
-  // function speak() {
-  //   console.log(msg.voice)
-  //   // console.log(voices(currSpeaker))
-  //   setSpeaker(Math.floor(Math.random() * voices.length))
-  //   console.log('speaker', speaker)
-  //   // console.log(currSpeaker)
-  //   setSpeech(`My name is`)
-  //   console.log('speech', speech)
-  // msg.voice = voices[Math.floor(Math.random() * voices.length)];
-  // msg.text = "I am speaking";
-  // window.speechSynthesis.speak(msg);
-  //   setLoading(false)
-  // }
-
-  // speechSynthesis.getVoices().forEach(function (voice, i) {
-  //   console.log(i, voice.name, voice.default ? voice.default : '');
-  // });
 
 
   return (
