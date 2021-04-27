8d3beab0b73efc245b3bc3740e41513d3ee053fd
Jonny van Essen <jonathan.vanessen@gmail.com>
2021-04-27 10:51:15 -0400

    testing testing

--- src/Trump.js
+++ src/Trump.js
@@ -1,22 +1,73 @@
-import React from 'react';
-import Speech from 'react-speech';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
+
+
 function Trump(props) {
+  const [speaker, setSpeaker] = useState([])
+  // const [speech, setSpeech] = useState('')
+  // const [loading, setLoading] = useState(false)
+  let msg;
+  let voices;
+
+  async function getSpeakers() {
+    msg = new SpeechSynthesisUtterance();
+    voices = await window.speechSynthesis.getVoices();
+  }
+
+  useEffect(() => {
+    getSpeakers()
+  }, [])
+
+  function speak() {
+    msg.voice = voices[Math.floor(Math.random() * voices.length)];
+    msg.text = "I am speaking";
+    window.speechSynthesis.speak(msg);
+  }
+
+
+
+  // let voices = window.speechSynthesis.getVoices();
+  // window.speechSynthesis.onvoiceschanged = function () {
+
+  // }
+  // setSpeaker(msg.voices)
+
+  // setTimeout(() => {
+  //   console.log(msg);
+  //   console.log(voices);
+  // }, 500)
 
 
 
 
+
+
+
+
+  // function speak() {
+  //   console.log(msg.voice)
+  //   // console.log(voices(currSpeaker))
+  //   setSpeaker(Math.floor(Math.random() * voices.length))
+  //   console.log('speaker', speaker)
+  //   // console.log(currSpeaker)
+  //   setSpeech(`My name is`)
+  //   console.log('speech', speech)
+  // msg.voice = voices[Math.floor(Math.random() * voices.length)];
+  // msg.text = "I am speaking";
+  // window.speechSynthesis.speak(msg);
+  //   setLoading(false)
+  // }
+
+  // speechSynthesis.getVoices().forEach(function (voice, i) {
+  //   console.log(i, voice.name, voice.default ? voice.default : '');
+  // });
 
 
   return (
     <div>
       <h1>Speech Ting</h1>
-      <Speech
-        textAsButton={true}
-        displayText="Hello"
-        text="I have text displayed as a button" />
-
+      <button onClick={speak}>Speech</button>
     </div>
   );
 }
