f78aad056079250b29db3bb21453790e358da26a
Hunter Cogan <robert.h.cogan@gmail.com>
2021-04-27 11:25:21 -0400

    randomize voice

--- src/Trump.js
+++ src/Trump.js
@@ -4,65 +4,21 @@ import axios from 'axios'
 
 
 function Trump(props) {
-  const [speaker, setSpeaker] = useState([])
-  // const [speech, setSpeech] = useState('')
-  // const [loading, setLoading] = useState(false)
-  let msg;
+
   let voices;
+  let msg = new SpeechSynthesisUtterance();
 
-  async function getSpeakers() {
-    msg = new SpeechSynthesisUtterance();
-    voices = await window.speechSynthesis.getVoices();
-  }
-
-  useEffect(() => {
-    getSpeakers()
-  }, [])
+  speechSynthesis.addEventListener("voiceschanged", () => {
+    voices = speechSynthesis.getVoices();
+  });
 
   function speak() {
-    msg.voice = voices[Math.floor(Math.random() * voices.length)];
-    msg.text = "I am speaking";
-    window.speechSynthesis.speak(msg);
+    if (voices.length) {
+        msg.voice = voices[Math.floor(Math.random() * voices.length)];
+        msg.text = "I am speaking";
+        window.speechSynthesis.speak(msg);
+    }
   }
-
-
-
-  // let voices = window.speechSynthesis.getVoices();
-  // window.speechSynthesis.onvoiceschanged = function () {
-
-  // }
-  // setSpeaker(msg.voices)
-
-  // setTimeout(() => {
-  //   console.log(msg);
-  //   console.log(voices);
-  // }, 500)
-
-
-
-
-
-
-
-
-  // function speak() {
-  //   console.log(msg.voice)
-  //   // console.log(voices(currSpeaker))
-  //   setSpeaker(Math.floor(Math.random() * voices.length))
-  //   console.log('speaker', speaker)
-  //   // console.log(currSpeaker)
-  //   setSpeech(`My name is`)
-  //   console.log('speech', speech)
-  // msg.voice = voices[Math.floor(Math.random() * voices.length)];
-  // msg.text = "I am speaking";
-  // window.speechSynthesis.speak(msg);
-  //   setLoading(false)
-  // }
-
-  // speechSynthesis.getVoices().forEach(function (voice, i) {
-  //   console.log(i, voice.name, voice.default ? voice.default : '');
-  // });
-
 
   return (
     <div>
